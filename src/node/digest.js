8001bb46599db44c95e5baebfd317a3c662fbbfd
Ryan Bennett <nomad.ry@gmail.com>
2015-07-17 16:54:40 -0600

    add SHA-256 digest

--- src/node/digest.js
+++ src/node/digest.js
@@ -1,7 +1,17 @@
-var digest = function digest(){
-  return new Promise(function rejecter(resolve,reject){
-    reject(new Error("operation not supported"));
-  });
+var forge = require("node-forge")
+var subtleToForge = {
+  "SHA-256" : "sha256"
+}
+
+var digest = function digest(alg, data){
+  if (!subtleToForge[alg.name])
+    return Promise.reject("unsupported hashing algorithm")
+  else{
+    var md = forge.md[subtleToForge[alg.name]].create()
+    md.update(data.toString("binary"))
+
+    return new Buffer(md.digest().bytes(), "binary")
+  }
 };
 
 module.exports = digest;
