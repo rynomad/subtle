97948e4e9620315e1a587129b68fdf9e1afa3197
Ryan Bennett <nomad.ry@gmail.com>
2015-07-27 14:01:36 -0600

    remove log

--- src/node/exportKey.js
+++ src/node/exportKey.js
@@ -1,6 +1,5 @@
 var exportKey = function exportKey(format, key, nonce){
   var args = arguments;
-  console.log('exportKey args', nonce)
   return new Promise(function rejecter(resolve,reject){
     if (!key._export)
       reject(new Error("operation not supported"));
