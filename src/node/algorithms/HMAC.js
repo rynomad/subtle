e0daabf423b9ae42e7c12a09f1643ddc6885f84c
Ryan Bennett <nomad.ry@gmail.com>
2015-07-27 13:58:35 -0600

    refactor HMAC to new style

--- src/node/algorithms/HMAC.js
+++ src/node/algorithms/HMAC.js
@@ -2,23 +2,28 @@ var CryptoKey  = require("../CryptoKey.js")
   , Algorithm  = require("./abstract")("HMAC")
   , NodeCrypto = require("crypto")
   , forge      = require("node-forge")
-  , private    = Algorithm.types.private.usage
-  , public     = Algorithm.types.public.usage;
+  , secret     = Algorithm.types.secret.usage
+  , raw        = Algorithm.formats.raw;
 
-private.sign  = createSign;
-public.verify = createVerify;
+module.exports = Algorithm;
 
-function createSign(key, hash){
+secret.sign    = createSign;
+secret.verify  = createVerify;
+
+raw.import = raw_import;
+
+function createSign(key,alg1){
   return function HMAC_SIGN(alg, buf){
-    var hashKey = alg.hash.name.replace(/-/g, '').toLowerCase();
+    var hashKey = alg1.hash.name.replace(/-/g, '').toLowerCase();
     return NodeCrypto.createHmac(hashKey, key)
                       .update(buf)
                       .digest();
   };
 }
 
-function createVerify(key, hash){
+function createVerify(key, alg1){
   return function HMAC_VERIFY(alg, buf, sig){
+    var hashKey = alg1.hash.name.replace(/-/g, '').toLowerCase();
     return (!Buffer.compare(sig, makeUsage["sign"](key, hash)(alg, buf)));
   };
 }
